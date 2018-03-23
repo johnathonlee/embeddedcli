63ccfe191084761b1fde985d491bb13394b0d9a2
jolee <jolee@redhat.com>
2018-03-23 15:35:32 -0500

    try embedded

--- extensions/install.sh
+++ extensions/install.sh
@@ -5,9 +5,6 @@ echo "XXX install.sh"
 injected_dir=$1
 cp -rf ${injected_dir} $JBOSS_HOME/extensions
 
-echo Start EAP in admin only mode and run some arbitrary command prior to the image service starting.
-/opt/eap/bin/standalone.sh --admin-only
-wait 10
 
 echo jboss-cli.sh to run script
-/opt/eap/bin/jboss-cli.sh --connect --file=actions.cli
+/opt/eap/bin/jboss-cli.sh --file=actions.cli
