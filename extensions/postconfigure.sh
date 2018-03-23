6a5e29d5591fd92f673da4233799a2e8247cacdc
jolee <jolee@redhat.com>
2018-03-23 16:03:45 -0500

    move

--- extensions/postconfigure.sh
+++ extensions/postconfigure.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-echo Start EAP in admin only mode and run some arbitrary command prior to the image service starting.
-/opt/eap/bin/standalone.sh --admin-only
-wait 10
+echo jboss-cli.sh to run script in extensions
+/opt/eap/bin/jboss-cli.sh --file=/opt/eap/extensions/actions.cli
 
-echo jboss-cli.sh to run script
-/opt/eap/bin/jboss-cli.sh --connect --file=actions.cli
 
