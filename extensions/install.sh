2323dcaa1f370fc1aa2e4844dd702f98f2fc0409
jolee <jolee@redhat.com>
2018-03-23 15:49:14 -0500

    next

--- extensions/install.sh
+++ extensions/install.sh
@@ -6,5 +6,5 @@ injected_dir=$1
 cp -rf ${injected_dir} $JBOSS_HOME/extensions
 
 
-echo jboss-cli.sh to run script
-/opt/eap/bin/jboss-cli.sh --file=actions.cli
+echo jboss-cli.sh to run script in extensions
+/opt/eap/bin/jboss-cli.sh --file=/opt/eap/extensions/actions.cli
