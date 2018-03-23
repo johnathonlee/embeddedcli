005253b2db52af87ca8bff370ea8bc8252ad0b79
jolee <jolee@redhat.com>
2018-03-23 15:59:47 -0500

    diagnostics for config

--- extensions/install.sh
+++ extensions/install.sh
@@ -4,7 +4,9 @@ set -x
 echo "XXX install.sh"
 injected_dir=$1
 cp -rf ${injected_dir} $JBOSS_HOME/extensions
-
+echo -------------CONFIG START----------------
+cat /opt/eap/standalone/configuration/standalone-openshift.xml
+echo -------------CONFIG END----------------
 
 echo jboss-cli.sh to run script in extensions
 /opt/eap/bin/jboss-cli.sh --file=/opt/eap/extensions/actions.cli
