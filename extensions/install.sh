4f6cc5817bb150a9dc3237820650540408724f31
jolee <jolee@redhat.com>
2018-03-26 09:50:41 -0500

    add datasource

--- extensions/install.sh
+++ extensions/install.sh
@@ -7,6 +7,3 @@ cp -rf ${injected_dir} $JBOSS_HOME/extensions
 echo -------------CONFIG START----------------
 cat /opt/eap/standalone/configuration/standalone-openshift.xml
 echo -------------CONFIG END----------------
-
-#echo jboss-cli.sh to run script in extensions
-#/opt/eap/bin/jboss-cli.sh --file=/opt/eap/extensions/actions.cli
